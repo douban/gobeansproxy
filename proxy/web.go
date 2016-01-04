053b323f71228210143dccd28737e1c15962c6de
zhuzhaolong <zhuzhaolong0@gmail.com>
2016-01-04 19:38:36 +0800

    Add proxy internal stats.

--- proxy/web.go
+++ proxy/web.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	_ "net/http/pprof"
 	"path/filepath"
+	"runtime"
 	"sync"
+	"syscall"
 	"text/template"
+
+	"github.intra.douban.com/coresys/gobeansdb/cmem"
+	mc "github.intra.douban.com/coresys/gobeansdb/memcache"
+	"github.intra.douban.com/coresys/gobeansdb/utils"
 
 	"github.intra.douban.com/coresys/gobeansproxy/dstore"
 )
+
+func handleWebPanic(w http.ResponseWriter) {
+	r := recover()
+	if r != nil {
+		stack := utils.GetStack(2000)
+		logger.Errorf("web req panic:%#v, stack:%s", r, stack)
+		fmt.Fprintf(w, "\npanic:%#v, stack:%s", r, stack)
+	}
+}
+
+func handleJson(w http.ResponseWriter, v interface{}) {
+	defer handleWebPanic(w)
+	b, err := json.Marshal(v)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	} else {
+		w.Write(b)
+	}
+}
 
 type templateHandler struct {
 	once     sync.Once
@@ -18,17 +45,35 @@ type templateHandler struct {
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join(proxyConf.StaticDir, t.filename)))
+		t.templ = template.Must(template.New("base.html").Option("missingkey=error").ParseFiles(
+			filepath.Join(proxyConf.StaticDir, t.filename),
+			filepath.Join(proxyConf.StaticDir, "templates/base.html")))
 	})
-	data := map[string]interface{}{
-		"stats": dstore.GetScheduler().Stats(),
+	var data map[string]interface{}
+	if t.filename == "templates/score.html" {
+		data = map[string]interface{}{
+			"stats": dstore.GetScheduler().Stats(),
+		}
 	}
-	t.templ.Execute(w, data)
+	e := t.templ.Execute(w, data)
+	if e != nil {
+		logger.Errorf("ServerHTTP filename:%s, error: %s", t.filename, e.Error())
+	}
 }
 
 func startWeb() {
-	http.Handle("/", &templateHandler{filename: "templates/index.html"})
 	http.Handle("/templates/", http.FileServer(http.Dir(proxyConf.StaticDir)))
+
+	http.Handle("/", &templateHandler{filename: "templates/score.html"})
+	http.Handle("/score/", &templateHandler{filename: "templates/score.html"})
+	http.Handle("/stats/", &templateHandler{filename: "templates/stats.html"})
+
+	http.HandleFunc("/config/", handleConfig)
+	http.HandleFunc("/requests/", handleRequests)
+	http.HandleFunc("/buffers/", handleBuffers)
+	http.HandleFunc("/memstats/", handleMemStates)
+	http.HandleFunc("/rusage/", handleRusage)
+
 	webaddr := fmt.Sprintf("%s:%d", proxyConf.Listen, proxyConf.WebPort)
 	go func() {
 		logger.Infof("HTTP listen at %s", webaddr)
@@ -36,4 +81,29 @@ func startWeb() {
 			logger.Fatalf("ListenAndServer: %s", err.Error())
 		}
 	}()
+}
+
+func handleConfig(w http.ResponseWriter, r *http.Request) {
+	handleJson(w, proxyConf)
+}
+
+func handleRequests(w http.ResponseWriter, r *http.Request) {
+	handleJson(w, mc.RL)
+}
+
+func handleRusage(w http.ResponseWriter, r *http.Request) {
+	var rusage syscall.Rusage
+	syscall.Getrusage(syscall.RUSAGE_SELF, &rusage)
+	handleJson(w, rusage)
+}
+
+func handleMemStates(w http.ResponseWriter, r *http.Request) {
+	var ms runtime.MemStats
+	runtime.ReadMemStats(&ms)
+	handleJson(w, ms)
+}
+
+func handleBuffers(w http.ResponseWriter, r *http.Request) {
+	defer handleWebPanic(w)
+	handleJson(w, &cmem.DBRL)
 }
