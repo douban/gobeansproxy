ec36f8ac5b8370bca92037578121d77ef50f9399
yangxiufeng <yangxiufeng@douban.com>
2016-04-12 16:45:15 +0800

    use new way of load/reload route from zk

--- gobeansproxy/web.go
+++ gobeansproxy/web.go
@@ -7,6 +7,7 @@ import (
 	_ "net/http/pprof"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"sync"
 	"text/template"
 	"time"
@@ -87,6 +88,7 @@ func startWeb() {
 	http.HandleFunc("/memstat/", handleMemStat)
 	http.HandleFunc("/rusage/", handleRusage)
 	http.HandleFunc("/route/", handleRoute)
+	http.HandleFunc("/route/version", handleRouteVersion)
 	http.HandleFunc("/route/reload", handleRouteReload)
 
 	webaddr := fmt.Sprintf("%s:%d", proxyConf.Listen, proxyConf.WebPort)
@@ -137,10 +139,29 @@ func handleRoute(w http.ResponseWriter, r *http.Request) {
 	handleYaml(w, config.Route)
 }
 
+func handleRouteVersion(w http.ResponseWriter, r *http.Request) {
+	defer handleWebPanic(w)
+	if len(proxyConf.ZKServers) == 0 {
+		w.Write([]byte("-1"))
+		return
+	} else {
+		w.Write([]byte(strconv.Itoa(dbcfg.ZKClient.Version)))
+	}
+}
+
+func getFormValueInt(r *http.Request, name string, ndefault int) (n int, err error) {
+	n = ndefault
+	s := r.FormValue(name)
+	if s != "" {
+		n, err = strconv.Atoi(s)
+	}
+	return
+}
+
 func handleRouteReload(w http.ResponseWriter, r *http.Request) {
 	var err error
 	if !dbcfg.AllowReload {
-		w.Write([]byte("reloading"))
+		w.Write([]byte("err: reloading"))
 		return
 	}
 
@@ -155,20 +176,24 @@ func handleRouteReload(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	if len(proxyConf.ZKServers) == 0 {
-		w.Write([]byte("not using zookeeper"))
+		w.Write([]byte("err: not using zookeeper"))
 		return
 	}
 
 	defer handleWebPanic(w)
-	newRouteContent, stat, err := dbcfg.ZKClient.GetRouteRaw()
+
+	r.ParseForm()
+	ver, err := getFormValueInt(r, "ver", -1)
 	if err != nil {
 		return
 	}
-	if dbcfg.ZKClient.Stat != nil && stat.Version == dbcfg.ZKClient.Stat.Version {
-		w.Write([]byte(fmt.Sprintf("same version %d", stat.Version)))
+
+	newRouteContent, ver, err := dbcfg.ZKClient.GetRouteRaw(ver)
+	if ver == dbcfg.ZKClient.Version {
+		w.Write([]byte(fmt.Sprintf("warn: same version %d", ver)))
 		return
 	}
-	info := fmt.Sprintf("update with route version %d\n", stat.Version)
+	info := fmt.Sprintf("update with route version %d\n", ver)
 	logger.Infof(info)
 	newRoute := new(dbcfg.RouteTable)
 	err = newRoute.LoadFromYaml(newRouteContent)
@@ -179,8 +204,8 @@ func handleRouteReload(w http.ResponseWriter, r *http.Request) {
 	oldScheduler := dstore.GetScheduler()
 	dstore.InitGlobalManualScheduler(newRoute, proxyConf.N)
 	config.Route = newRoute
-	dbcfg.ZKClient.Stat = stat
-	w.Write([]byte("success"))
+	dbcfg.ZKClient.Version = ver
+	w.Write([]byte("ok"))
 
 	go func() {
 		// sleep for request to be completed.
