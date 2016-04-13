254f018d8c9b867dcf4eb25ce79e55d42b9a08e1
yangxiufeng <yangxiufeng@douban.com>
2016-04-13 16:36:58 +0800

    update gobeansdb

--- vendor/github.intra.douban.com/coresys/gobeansdb/config/zk.go
+++ vendor/github.intra.douban.com/coresys/gobeansdb/config/zk.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/samuel/go-zookeeper/zk"
@@ -15,7 +16,7 @@ var (
 )
 
 type zkClient struct {
-	Stat    *zk.Stat
+	Version int
 	Root    string
 	Servers []string
 	Client  *zk.Conn
@@ -29,11 +30,32 @@ func NewZK(root string, servers []string) (c *zkClient, err error) {
 }
 
 func (c *zkClient) Get(subPath string) ([]byte, *zk.Stat, error) {
-	return c.Client.Get(fmt.Sprintf("%s/%s", c.Root, subPath))
+	path := fmt.Sprintf("%s/%s", c.Root, subPath)
+	log.Printf("get zk %s", path)
+	return c.Client.Get(path)
 }
 
-func (c *zkClient) GetRouteRaw() ([]byte, *zk.Stat, error) {
-	return c.Get("route")
+func (c *zkClient) GetRouteRaw(version int) (data []byte, ver int, err error) {
+	if version < 0 {
+		data, _, err = c.Get("route")
+		if err != nil {
+			log.Printf("%v", err)
+			return
+		}
+		log.Printf("got route version %s", string(data))
+		ver, err = strconv.Atoi(string(data))
+		if err != nil {
+			return
+		}
+	} else {
+		ver = version
+	}
+	sp := fmt.Sprintf("route/route_%010d", ver)
+	data, _, err = c.Get(sp)
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	return
 }
 
 func UpdateLocalRoute(content []byte) {
