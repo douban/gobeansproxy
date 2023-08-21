f8397b564ad1bdf525863f06c159e7569740d6e5
mu <sunsetmask@gmail.com>
2023-08-21 15:57:17 +0800

    Add cfg check for prefix dup

--- cassandra/prefix_table_finder.go
+++ cassandra/prefix_table_finder.go
@@ -24,9 +24,12 @@ type KeyTableFinder struct {
 	lock sync.RWMutex
 }
 
-func getTableTrieFromCfg(config *config.CassandraStoreCfg) trie.Tree[rune, string] {
+func getTableTrieFromCfg(config *config.CassandraStoreCfg) (*trie.Tree[rune, string], error) {
 	t2k := config.TableToKeyPrefix
 	var ptrie trie.Tree[rune, string]
+
+	dedup := map[string]struct{}{}
+
 	if len(t2k) > 0 {
 		runesKeys := [][]rune{}
 		strValues := []string{}
@@ -35,18 +38,27 @@ func getTableTrieFromCfg(config *config.CassandraStoreCfg) trie.Tree[rune, strin
 			for _, prefix := range kprefixs {
 				runesKeys = append(runesKeys, []rune(prefix))
 				strValues = append(strValues, t)
+				if _, ok := dedup[prefix]; !ok {
+					dedup[prefix] = struct{}{}
+				} else {
+					// prefix can map to only one status
+					return nil, fmt.Errorf("%s duplicate in table find settings", prefix)
+				}
 			}
 		}
     
 		ptrie = trie.New[rune, string](runesKeys, strValues)
 	}
-	return ptrie
+	return &ptrie, nil
 }
 
 func NewKeyTableFinder(config *config.CassandraStoreCfg) (*KeyTableFinder, error) {
 	f := new(KeyTableFinder)
-	t := getTableTrieFromCfg(config)
-	f.trie = &t
+	t, err := getTableTrieFromCfg(config)
+	if err != nil {
+		return nil, err
+	}
+	f.trie = t
 	f.defaultT = config.DefaultTable
 
 	// init sql str
@@ -123,10 +135,13 @@ func (f *KeyTableFinder) LoadCfg(cfgDir string) error {
 	}
 
 	logger.Infof("reloading c* cfg for table finder to: %v", cfg.CassandraCfg.TableToKeyPrefix)
-	pTrie := getTableTrieFromCfg(&cfg.CassandraCfg)
+	pTrie, err := getTableTrieFromCfg(&cfg.CassandraCfg)
+	if err != nil {
+		return err
+	}
 
 	f.lock.Lock()
 	defer f.lock.Unlock()
-	f.trie = &pTrie
+	f.trie = pTrie
 	return nil
 }
