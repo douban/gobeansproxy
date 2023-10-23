d7b668a50e8fe514e809d9e233e57c77e209b6e0
mu <sunsetmask@gmail.com>
2023-10-23 12:45:40 +0800

    Improve prometheus histogram accuracy

--- dstore/metrics.go
+++ dstore/metrics.go
@@ -36,24 +36,17 @@ func init() {
 	)
 	BdbProxyPromRegistry.MustRegister(errorReqs)
 	
-	cmdReqDurationSeconds = prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Namespace: "gobeansproxy",
-			Name: "cmd_req_duration_seconds",
-			Help: "cmd req duration",
-			Buckets: []float64{0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.1, 0.25, 0.5, 1, 2, 5},
-		},
-
-		[]string{"cmd", "store"},
-	)
-	BdbProxyPromRegistry.MustRegister(cmdReqDurationSeconds)
-
 	cmdE2EDurationSeconds = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Namespace: "gobeansproxy",
 			Name: "cmd_e2e_duration_seconds",
 			Help: "cmd e2e duration",
-			Buckets: []float64{0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.1, 0.25, 0.5, 1, 2, 5},
+			Buckets: []float64{
+				0.001, 0.003, 0.005,
+				0.01, 0.03, 0.05, 0.07,
+				0.1, 0.3, 0.5, 0.7,
+				1, 2, 5,
+			},
 		},
 
 		[]string{"cmd"},
